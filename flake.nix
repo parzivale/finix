e7ed70ba819719887156e1a1aacbb4fcb84db7f4
parzivale <zeus@theolivers.org>
2026-05-31 01:10:30 +0200

    pinned input :)

--- flake.nix
+++ flake.nix
@@ -1,6 +1,10 @@
 {
   description = "A collection of overlays, modules, libs, and templates for working with finix";
 
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/d233902339c02a9c334e7e593de68855ad26c4cb";
+  };
+
   outputs =
     { self }:
     {
